8abdd673fb839a9cc08925b7f7e57ea0d816188e
vettelistrue <enes.kulatay@gmail.com>
2025-06-14 19:13:42 +0300

    Update vavoo.py

--- Pythonlar/vavoo.py
+++ Pythonlar/vavoo.py
@@ -2,7 +2,7 @@ import requests
 import re
 
 URL = "https://vavoo.to/channels"
-PROXY_BASE = "https://vettelchannelowner-vettel-channel.hf.space/proxy/m3u?url=https://vavoo.to/play/{}/index.m3u8"
+PROXY_BASE = "https://vettelchannel-realvettelchannel.hf.space/proxy/m3u?url=https://vavoo.to/play/{}/index.m3u8"
 LOGO_URL = "https://raw.githubusercontent.com/kerimmkirac/CanliTvListe/refs/heads/main/vavoo.png"
 OUTPUT_FILE = "vavoo.m3u"
 
