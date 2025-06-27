a86d53b9b3e7c7cf723768cc0f74cbbb747f2824
vettelistrue <enes.kulatay@gmail.com>
2025-06-27 14:55:35 +0300

    Update kralbet.py

--- Pythonlar/kralbet.py
+++ Pythonlar/kralbet.py
@@ -19,10 +19,15 @@ with open(M3U_FILE, "w", encoding="utf-8") as f:
     f.write("#EXTM3U\n\n")
     for idx, channel in enumerate(channels):
         href = channel.get("href")
+        stream_url = f"{KRAL_BET}/{href}"
+
+        # 👇 ENGELLEME: Eğer stream_url 'channel?id=' içeriyorsa atla
+        if "channel?id=" in stream_url:
+            continue
+
         kanal_id = href.split("id=")[-1]
         tvg_name = titles[idx].text.strip() if idx < len(titles) else f"Kanal_{idx}"
         logo_url = f"{LINK_PREFIX}/{images[idx]['src'].lstrip('/')}" if idx < len(images) else ""
-        stream_url = f"{KRAL_BET}/{href}"
 
         f.write(
             f'#EXTINF:-1 tvg-name="{tvg_name}" tvg-language="Türkçe" tvg-country="Türkiye" '
